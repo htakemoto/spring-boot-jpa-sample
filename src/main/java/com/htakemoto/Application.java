fea4d0a5caf2a1c10c7431159657a183bf77f992
Hiroshi Takemoto <hiroshi.takemoto@outlook.com>
2016-03-26 16:16:35 -0400

    updated spring-boot version to 1.3.3

--- src/main/java/com/htakemoto/Application.java
+++ src/main/java/com/htakemoto/Application.java
@@ -1,13 +1,9 @@
 package com.htakemoto;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan("com.htakemoto")
-@EnableJpaRepositories
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
 	
     public static void main(String[] args) throws Exception {
